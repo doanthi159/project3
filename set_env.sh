18218f6724de4b27ebc6f65809c4ee920bff57f0
doanthi159 <doanthi2241@gmail.com>
2023-03-31 16:25:32 +0700

    init code

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=kccs0000
+export POSTGRES_HOST=database-1.cnkhbrslglqh.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=test-776345365315-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8080
